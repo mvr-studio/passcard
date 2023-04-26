ee9d15eaa728c3a7b071ce8a9d26d53fb888ddf6
Tomek Marciniak <tmarciniakm@gmail.com>
2023-04-26 09:19:09 +0200

    chore(lib): update docs

--- apps/docs/pages/_document.tsx
+++ apps/docs/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+        <script async src="https://umami.mvr.studio/script.js" data-website-id="69299e5b-0ef5-4cd0-98f9-c59e9a02c33a" />
       </Head>
       <body>
         <Main />
